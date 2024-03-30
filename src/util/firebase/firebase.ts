c62eabf43ea7f3140f45f84fe32539030646b0e5
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2024-03-30 16:21:08 -0500

    final commit

--- src/util/firebase/firebase.ts
+++ src/util/firebase/firebase.ts
@@ -14,11 +14,3 @@ export const firebaseConfig:FirebaseOptions = {
 };
 
 export const app = initializeApp(firebaseConfig);
-
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider(process.env.SITE_KEY ? process.env.SITE_KEY : ""),
-
-  // Optional argument. If true, the SDK automatically refreshes App Check
-  // tokens as needed.
-  isTokenAutoRefreshEnabled: true
-});
